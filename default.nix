3d3a0a53475dd7644a091e96c9d30ce8dfd81251
Shitikov Kirill <shitikovkirillt@gmail.com>
2025-08-31 14:28:31 +0200

    Fix permisions

--- default.nix
+++ default.nix
@@ -9,6 +9,18 @@
       filter = "*rpi-4-*.dtb";
     };
   };
+
   console.enable = false;
   environment.systemPackages = with pkgs; [ libraspberrypi raspberrypi-eeprom ];
+
+  # Create gpio group
+  users.groups.gpio = { };
+
+  # Change permissions gpio devices
+  services.udev.extraRules = ''
+    SUBSYSTEM=="bcm2835-gpiomem", KERNEL=="gpiomem", GROUP="gpio",MODE="0660"
+    SUBSYSTEM=="gpio", KERNEL=="gpiochip*", ACTION=="add", RUN+="${pkgs.bash}/bin/bash -c 'chown root:gpio /sys/class/gpio/export /sys/class/gpio/unexport ; chmod 220 /sys/class/gpio/export /sys/class/gpio/unexport'"
+    SUBSYSTEM=="gpio", KERNEL=="gpio*", ACTION=="add",RUN+="${pkgs.bash}/bin/bash -c 'chown root:gpio /sys%p/active_low /sys%p/direction /sys%p/edge /sys%p/value ; chmod 660 /sys%p/active_low /sys%p/direction /sys%p/edge /sys%p/value'"
+  '';
+
 }
